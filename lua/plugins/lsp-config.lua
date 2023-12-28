a9c3ce668459ee3bbc17683f0e02e14e328e7782
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-28 00:57:57 -0800

    groovy is working

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "tsserver" }
+      ensure_installed = { "lua_ls", "tsserver", "groovyls" }
     },
     config = function(_, opts)
       require("mason-lspconfig").setup(opts)
@@ -24,18 +24,26 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+      lspconfig.groovyls.setup({
+        cmd = { "java", "-jar", "/home/skylli/.local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar" },
+        settings = {
+          groovy = {
+            classpath = {
+              "/home/skylli/.sdkman/candidates/groovy/current/lib",
+            },
+          },
+        }
+      })
 
       -- Keymaps
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-      vim.keymap.set('n', '<leader>f', function()
-        vim.lsp.buf.format { async = true }
-      end, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {})
     end
   }
 }
