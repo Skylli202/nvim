31c6d84d4b00f1523efd8f8a2a13cb8782310f6e
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-12-30 19:52:27 -0800

    auto-completion (friendly snippet disabled tho)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,54 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = {
-      { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" }
-    },
-    config = function(_, opts)
-      require("mason").setup(opts)
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "lua_ls", "tsserver", "groovyls" }
-    },
-    config = function(_, opts)
-      require("mason-lspconfig").setup(opts)
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function(_, opts)
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.groovyls.setup({
-        cmd = { "java", "-jar", "/home/skylli/.local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar" },
-        settings = {
-          groovy = {
-            classpath = {
-              "/home/skylli/.sdkman/candidates/groovy/current/lib",
-            },
-          },
-        }
-      })
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = {
+			{ "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
+		},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "groovyls", "html", "lua_ls", "svelte", "tsserver" },
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Keymaps
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {})
-    end
-  }
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.groovyls.setup({
+				capabilities = capabilities,
+				cmd = {
+					"java",
+					"-jar",
+					"/home/skylli/.local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar",
+				},
+				settings = {
+					groovy = {
+						classpath = {
+							"/home/skylli/.sdkman/candidates/groovy/current/lib",
+						},
+					},
+				},
+			})
+
+			-- Keymaps
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+		end,
+	},
 }
